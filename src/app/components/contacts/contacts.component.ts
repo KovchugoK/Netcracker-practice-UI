5e9b17d53b4ea31ed769650cc797fa76af17dc21
Yauheni Harkavik <bugatti1701@gmail.com>
2018-12-17 21:11:44 +0300

    Added spinner for contacts

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -3,10 +3,12 @@ import {ActivatedRoute} from '@angular/router';
 import {ContactsService} from '../../services/contacts.service';
 import {Contact} from '../../model/Contact';
 import {NgRedux, select} from '@angular-redux/store';
-import {selectContacts} from '../../store/selectors/contacts.selector';
+import {isLoading, selectContacts} from '../../store/selectors/contacts.selector';
 import {Observable} from 'rxjs';
 import {AppState} from '../../store';
 import {deleteContactAction, fetchContactsAction} from '../../store/actions/contacts.actions';
+import {skipWhile, take} from 'rxjs/operators';
+import {searchStartupsAction} from '../../store/actions/startups.actions';
 
 @Component({
   selector: 'app-contacts',
@@ -17,14 +19,23 @@ export class ContactsComponent implements OnInit {
   @select(selectContacts)
   contacts: Observable<Contact[]>;
 
+  @select(isLoading)
+  isLoading: Observable<boolean>;
+
   constructor(private contactService: ContactsService, private route: ActivatedRoute, private ngRedux: NgRedux<AppState>) {
   }
 
   ngOnInit() {
     /*this.contactService.getUserContacts(this.route.snapshot.paramMap.get('id'))
       .subscribe(contacts => this.contacts = contacts);*/
-    this.ngRedux.dispatch(fetchContactsAction(this.route.snapshot.paramMap.get('id')));
-    this.ngRedux.select(selectContacts);
+    this.isLoading.pipe(skipWhile(result => result === true), take(1))
+      .subscribe(() =>
+        this.ngRedux.dispatch(this.ngRedux.dispatch(fetchContactsAction(this.route.snapshot.paramMap.get('id')))));
+
+    this.isLoading.pipe(skipWhile(result => result === true), take(1))
+      .subscribe(() => this.ngRedux.select(selectContacts));
+    //this.ngRedux.dispatch(fetchContactsAction(this.route.snapshot.paramMap.get('id')));
+    //this.ngRedux.select(selectContacts);
   }
 
   deleteContact(otherId: string): void {
--- src/app/store/reducers/contacts.reducer.ts
+++ src/app/store/reducers/contacts.reducer.ts
@@ -11,34 +11,36 @@ import {
 
 export interface ContactsState {
   readonly contacts: Map<string, Contact>;
+  readonly isLoading: boolean;
 }
 
 const INITIAL_STATE = {
-  contacts: new Map<string, Contact>()
+  contacts: new Map<string, Contact>(),
+  isLoading: false
 };
 
 export const contactsReducer: Reducer<ContactsState> = (state: ContactsState = INITIAL_STATE, action): ContactsState => {
   switch (action.type) {
     case FETCH_CONTACTS:
-      return {...state, ...action.payload};
+      return {...state, isLoading: true};
     case FETCH_CONTACTS_SUCCESS:
-      return {...state, ...action.payload};
+      return {...state, ...action.payload, isLoading: false};
     case DELETE_CONTACT:
-      return {...state, ...action.payload};
+      return {...state, isLoading: true};
     case DELETE_CONTACT_SUCCESS: {
       const updatedContacts = new Map(state.contacts);
       updatedContacts.delete(action.payload.otherId);
-      return {...state, contacts: updatedContacts};
+      return {...state, contacts: updatedContacts, isLoading: false};
     }
     case DELETE_CONTACT_FAILED:
       return {...state, ...action.payload};
     case ADD_CONTACT:
-      return {...state, ...action.payload};
+      return {...state, isLoading: false};
     case ADD_CONTACT_SUCCESS: {
       const {contact} = action.payload;
       const updatedContacts = new Map(state.contacts);
       updatedContacts.set(contact.id, contact);
-      return {...state, contacts: updatedContacts};
+      return {...state, contacts: updatedContacts, isLoading: false};
     }
     case ADD_CONTACT_FAILED:
       return {...state, ...action.payload};
